6eb51520ea7a62204f79e8ef273c9b5e1ab05605
GeneralGrifon <56627540+GeneralGrifon@users.noreply.github.com>
2021-10-10 22:10:35 +0400

    Add files via upload

---
+++ sellfileeasy/interkassa/config.php
@@ -0,0 +1,14 @@
+<?php
+	//Shop ID
+	$ik_shop_id = '52e0105ebf4efc070d704c1d';
+	//Secret Key
+	$ik_secret_key = 'rVCrCFMt3Tdz2kzT';
+	//Initial Key(the same as ik_secret_key, or any key from interkassa Test field for test only)
+	$ik_initial_key = 'rVCrCFMt3Tdz2kzT';
+	//Description
+	$ik_desc = __("File for download");
+	//Currency
+	$ik_currency = 'RUB';
+	//Language
+	$ik_locale = 'ru';
+?>
